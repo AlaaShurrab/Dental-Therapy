8ac0eae43e6d2d962af0e72aa4798908aa2c0e54
AlaaShurrab <alaa.m.shurrab98@gmail.com>
2021-06-25 00:42:32 +0300

    creating the patient teeth therapy table relates #12

---
+++ server/src/database/models/patient_teeth_therapy/schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS "patient_teeth_therapy" CASCADE;
+
+CREATE TABLE "patient_teeth_therapy" (
+  "id" SERIAL PRIMARY KEY,
+  "patient_id" INTEGER REFERENCES patients(id) NOT NULL,
+  "teeth_therapy_id" INTEGER REFERENCES teeth_therapy(id) NOT NULL,
+  "tooth_lable" VARCHAR(5) NOT NULL,
+  "therapy_number" INTEGER NOT NULL,
+  "notes" TEXT,
+  "created_at" TIMESTAMP::timestamp::date,
+);
