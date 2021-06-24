318210c0619af3021875e2eeb5a8cf4291c66fa2
AlaaShurrab <alaa.m.shurrab98@gmail.com>
2021-06-25 00:26:39 +0300

    creating the sanitize csv injection function to format all the data before adding it to the database relates #12

---
+++ server/src/database/utils/sanitize-csv-injection.js
@@ -0,0 +1,42 @@
+const isArray = (a) => Array.isArray(a);
+
+const isObject = (o) =>
+  o === Object(o) && !isArray(o) && typeof o !== 'function';
+
+const possibleCsvInject = RegExp(/^(=|"=|\+|"\+|-|"-|@|"@)/);
+
+const sanitizeCSVInjection = (params) => {
+  if (isObject(params)) {
+    const n = {};
+
+    Object.entries(params).forEach(([key, _value]) => {
+      if (isArray(_value) || isObject(_value)) {
+        sanitizeCSVInjection(_value);
+      }
+
+      let value = _value;
+      if (possibleCsvInject.test(_value)) {
+        value = `\t${_value}`;
+      }
+      n[key] = value;
+    });
+
+    return n;
+  }
+
+  if (isArray(params)) {
+    return params.map((value) => sanitizeCSVInjection(value));
+  }
+
+  if (typeof params === 'string') {
+    let value = params;
+    if (possibleCsvInject.test(params)) {
+      value = `\t${params}`;
+    }
+    return value;
+  }
+
+  return params;
+};
+
+export default sanitizeCSVInjection;
