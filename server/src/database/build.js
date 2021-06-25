75caf48d7a16bd613a3447a65fd760867d24088f
AlaaShurrab <alaa.m.shurrab98@gmail.com>
2021-06-25 20:53:57 +0300

    creating the index file in the modules and creating the build .js file with all the tables and the init files relares #12

---
+++ server/src/database/build.js
@@ -0,0 +1,24 @@
+import * as models from './models';
+import init from './init';
+
+// import buildData from './dummy-data';
+
+// development data build
+const build = async () => {
+  await init.createTypes();
+  //   await init.buildMigrations();
+
+  await models.users.createTable();
+  await models.patients.createTable();
+  await models.appointments.createTable();
+  await models.generalTherapy.createTable();
+  await models.teethTherapy.createTable();
+  await models.patientGeneralTherapy.createTable();
+  await models.patientTeethTherapy.createTable();
+
+  // build dummy data
+  //   const createdData = await buildData();
+  //   return createdData;
+};
+
+export default build;
