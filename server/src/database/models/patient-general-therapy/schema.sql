194a6ee3037c6d7de2e6e6ecec6cf231994b8a80
AlaaShurrab <alaa.m.shurrab98@gmail.com>
2021-06-25 20:45:04 +0300

    changing the name of global therapy to general therapy and fixing some linting and style mistakes relates #12

--- server/src/database/models/patient-general-therapy/schema.sql
+++ server/src/database/models/patient-general-therapy/schema.sql
@@ -1,10 +1,10 @@
-DROP TABLE IF EXISTS "patient_global_therapy" CASCADE;
+DROP TABLE IF EXISTS "patient_general_therapy" CASCADE;
 
-CREATE TABLE "patient_global_therapy" (
+CREATE TABLE "patient_general_therapy" (
   "id" SERIAL PRIMARY KEY,
   "patient_id" INTEGER REFERENCES patients(id) NOT NULL,
   "global_therapy_id" INTEGER REFERENCES global_therapy(id) NOT NULL,
   "therapy_number" INTEGER NOT NULL,
   "notes" TEXT,
-  "created_at" TIMESTAMP::timestamp::date,
+  "created_at" TIMESTAMP NOT NULL DEFAULT NOW()
 );
