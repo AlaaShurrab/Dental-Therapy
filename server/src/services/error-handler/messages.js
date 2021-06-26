13903fa52c700bdcadd62fbda1e9bce27d398eea
Osama Wael Younis <osama.w.younis@gmail.com>
2021-06-26 10:12:55 +0300

    add same errorMsgs Relates #15

---
+++ server/src/services/error-handler/messages.js
@@ -0,0 +1,14 @@
+const errors = {
+  EMAIL_ALREADY_EXISTS: 'A user account with that email already exists',
+  USERNAME_ALREADY_EXISTS: 'A user account with that username already exists',
+  INVALID_EMAIL_OR_PASSWORD: 'Email and/or password are incorrect',
+  INVALID_USER_ROLE: 'invalid role',
+  VALIDATION_ERROR: 'There was an issue with your request (validation error)',
+  CUSTOM_ERROR_MESSAGE: 'error message for more details to user',
+  WRONG_DATA: 'something went wrong with matching your data',
+  UNAUTHORISED: 'You do not have permission to view this content',
+  UNAUTHORISED_EDIT: 'You do not have permission to do that',
+  NOT_FOUND: "We cannot find what you're looking for",
+};
+
+export default errors;
