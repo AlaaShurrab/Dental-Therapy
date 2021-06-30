77f7d1fe72e62c42e4bee7dd735e6e1acf2ed3e6
Osama Wael Younis <osama.w.younis@gmail.com>
2021-06-30 22:40:41 +0300

    add validation err-msgs Relates #17

---
+++ server/src/services/validation/err-msgs.js
@@ -0,0 +1,8 @@
+export const DEFAULT_REQUIRED = 'This field is required';
+export const FIELD_REQUIRED = (fieldName) =>
+  `The ${fieldName} field is required`;
+export const SHORT_PASSWORD =
+  'Password must contain min. 8 characters, one capital letter, one lowercase letter and one number';
+export const INVALID_EMAIL = 'Invalid email';
+export const INVALID_USERNAME = 'Invalid userName';
+export const INVALID_ROLE = 'Invalid role';
