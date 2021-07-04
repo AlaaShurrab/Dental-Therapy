9bb2e511c1bc02214afd83e0edd9de26bb915171
AlaaShurrab <alaa.m.shurrab98@gmail.com.com>
2021-07-04 13:32:50 +0300

    create the general tabel scema Relates #41

---
+++ server/src/database/models/general-settings/schema.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS "general_settings" CASCADE;
+
+CREATE TABLE "general_settings" (
+  "id" SERIAL PRIMARY KEY,
+  "open_time" TIME NOT NULL,
+  "working_hours" INTEGER NOT NULL,
+  "appointment_duration_in_minutes" INTEGER NOT NULL,
+  "update_at" TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON "general_settings"
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
