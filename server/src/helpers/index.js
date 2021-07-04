c89d1839624220f761141e8edff9426dc20d5571
Osama Wael Younis <osama.w.younis@gmail.com>
2021-07-04 15:25:37 +0300

    create isDateCurrentlyAvailable func helper

--- server/src/helpers/index.js
+++ server/src/helpers/index.js
@@ -1,3 +1,5 @@
 import verifyPassword from './verify-password';
+import isDateCurrentlyAvailable from './isDateCurrentlyAvailable';
+import createToken from './create-token';
 
-export { verifyPassword };
+export { verifyPassword, isDateCurrentlyAvailable, createToken };
