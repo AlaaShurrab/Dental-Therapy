2f6fe702214f37b15edaefa39c225e5091ee8379
Osama Wael Younis <osama.w.younis@gmail.com>
2021-07-04 18:00:22 +0300

    using the app setting for get availableAppointments Relates #40

--- server/src/modules/appointment/controllers/available-appointments.js
+++ server/src/modules/appointment/controllers/available-appointments.js
@@ -1,26 +1,35 @@
-// start, end, lime
 import moment from 'moment';
-import { isDateCurrentlyAvailable } from '../../../helpers';
+import Boom from '@hapi/boom';
 import * as Appointment from '../use-cases';
-import { availableAppointmentsName } from '../utils';
+import { useCases as settings } from '../../general-settings';
+import { isDateCurrentlyAvailable } from '../../../helpers';
+import * as errMsgs from '../../../services/validation/err-msgs';
+import { validateAvailableAppointments } from '../utils';
 
 const availableAppointments = async (req, res, next) => {
   const { targetedDate } = req.query;
   try {
-    await availableAppointmentsName({ targetedDate });
+    if (!targetedDate)
+      throw Boom.badData(errMsgs.FIELD_REQUIRED('targetedDate'));
 
-    // GET THE DATA
-    const data = {
-      start: '01:30',
-      duration: 6,
-      stepByMinute: 20,
-    };
+    await validateAvailableAppointments({ targetedDate });
+
+    const date = moment(targetedDate, 'DD-MM-YYYY');
+
+    const { openTime, workingHours, appointmentDurationInMinutes, daysOff } =
+      await settings.getGeneralSettings();
 
     let arr = [];
-    if (isDateCurrentlyAvailable(moment(targetedDate, 'DD-MM-YYYY'))) {
+
+    if (
+      isDateCurrentlyAvailable(date) &&
+      !daysOff.includes(date.format('dddd'))
+    ) {
       arr = await Appointment.availableByDate({
         targetedDate,
-        ...data,
+        openTime,
+        workingHours,
+        appointmentDurationInMinutes,
       });
     }
 
