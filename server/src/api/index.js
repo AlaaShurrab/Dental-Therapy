ce4e2aa7587e1a2fbd9c94055ceb553aac76db7b
Osama Wael Younis <osama.w.younis@gmail.com>
2021-07-04 15:30:08 +0300

    Add appointments router Relates #40

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import Boom from '@hapi/boom';
 import { errorMsgs } from '../services/error-handler';
-import { user } from '../modules';
+import { user, appointment } from '../modules';
 
 const router = Router();
 
 router.use('/users', user.controllers);
+router.use('/appointments', appointment.controllers);
 
 // catch 404 error and pass it to error handler
 router.use((req, res, next) => {
