ffa4801e40689fa1990bf436b12b642215b50bb9
Osama Wael Younis <osama.w.younis@gmail.com>
2021-07-05 22:04:37 +0300

    add firstName, lastName, id, and phoneNumber field Relates #45

--- server/src/services/validation/fields.js
+++ server/src/services/validation/fields.js
@@ -1,4 +1,4 @@
-import { string, date } from 'yup';
+import { string, number } from 'yup';
 import * as errMsgs from './err-msgs';
 
 export const email = string()
@@ -10,6 +10,28 @@ export const userName = string()
   .max(20, errMsgs.INVALID_USERNAME)
   .required(errMsgs.FIELD_REQUIRED('userName'));
 
+export const firstName = string()
+  .min(1, errMsgs.FIELD_REQUIRED('firstName'))
+  .max(20)
+  .required(errMsgs.FIELD_REQUIRED('firstName'));
+
+export const lastName = string()
+  .min(1, errMsgs.FIELD_REQUIRED('lastName'))
+  .max(20)
+  .required(errMsgs.FIELD_REQUIRED('lastName'));
+
+export const id = number()
+  .min(1)
+  .required(errMsgs.FIELD_REQUIRED('id'))
+  .typeError(errMsgs.FIELD_REQUIRED('id'));
+
+export const phoneNumber = string()
+  .matches(
+    /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/,
+    errMsgs.INVALID_PHONE
+  )
+  .required(errMsgs.FIELD_REQUIRED('phone number'));
+
 export const password = string()
   .matches(
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
@@ -17,4 +39,7 @@ export const password = string()
   )
   .required(errMsgs.FIELD_REQUIRED('password'));
 
-export const targetedDate = date().typeError(errMsgs.INVALID_DATE);
+export const time = string().matches(
+  /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:00$/,
+  errMsgs.INVALID_OPEN_TIME
+);
