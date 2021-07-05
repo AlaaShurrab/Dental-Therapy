d38cb9c7d8d10485e1b7e8edd4fb8990e4c54871
Osama Wael Younis <osama.w.younis@gmail.com>
2021-07-05 22:11:37 +0300

    create findPatientID query Relates #45

---
+++ server/src/modules/patient/model/find.js
@@ -0,0 +1,20 @@
+import { query } from '../../../database';
+
+const findPatientID = async ({ firstName, lastName, phone }, client) => {
+  const values = [firstName, lastName, phone];
+  const sql = `
+    SELECT
+      id
+    FROM 
+      patients
+    WHERE
+      first_name = $1 
+        AND last_name = $2 
+        AND phone = $3 ;
+  `;
+
+  const res = await query(sql, values, client);
+  return res.rows[0];
+};
+
+export { findPatientID };
