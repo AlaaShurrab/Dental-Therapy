20558b6d13a3efe95744f621d4f9e65d61f2299f
Osama Wael Younis <osama.w.younis@gmail.com>
2021-07-05 22:02:22 +0300

    create isValidDate helper Relates #45

--- server/src/helpers/index.js
+++ server/src/helpers/index.js
@@ -1,5 +1,17 @@
 import verifyPassword from './verify-password';
-import isDateCurrentlyAvailable from './isDateCurrentlyAvailable';
+import isDateCurrentlyAvailable from './is-date-currently-available';
 import createToken from './create-token';
+import timeFormatter from './time-formatter';
+import dateValidation from './date-validation';
+import getDayByDate from './get-day-by-date';
+import dateFormatter from './date-formatter';
 
-export { verifyPassword, isDateCurrentlyAvailable, createToken };
+export {
+  verifyPassword,
+  isDateCurrentlyAvailable,
+  createToken,
+  timeFormatter,
+  dateValidation,
+  getDayByDate,
+  dateFormatter,
+};
