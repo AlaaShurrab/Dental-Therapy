4c6e70ef6e0b2f4b62f4e468c04d78f0e164135c
Osama Wael Younis <osama.w.younis@gmail.com>
2021-07-05 22:07:39 +0300

    remove the date format inside the sql query Relates #45

--- server/src/modules/appointment/model/find.js
+++ server/src/modules/appointment/model/find.js
@@ -7,9 +7,8 @@ const findUnavailableTimes = async (date, client) => {
       appointment_time
 
     FROM appointments 
-      WHERE to_char(appointment_date,'DD-MM-YYYY') = $1
+      WHERE appointment_date = $1
   `;
-
   const res = await query(sql, values, client);
   return res.rows;
 };
