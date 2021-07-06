efc83b3da733cd7ae69d84d195e45ac39a6f6405
Osama Wael Younis <osama.w.younis@gmail.com>
2021-07-06 11:21:12 +0300

    fix spaces inside query

--- server/src/modules/general-settings/model/update.js
+++ server/src/modules/general-settings/model/update.js
@@ -14,12 +14,12 @@ const updateGeneralSettings = async (
     UPDATE
       general_settings
     SET 
-      days_off=COALESCE($1,days_off ),
-      open_time=COALESCE($2,open_time ),
-      working_hours=COALESCE($3,working_hours ),
-      appointment_duration_in_minutes=COALESCE($4,appointment_duration_in_minutes )
+      days_off = COALESCE($1, days_off),
+      open_time = COALESCE($2, open_time),
+      working_hours = COALESCE($3, working_hours),
+      appointment_duration_in_minutes = COALESCE($4, appointment_duration_in_minutes)
     WHERE
-      id=1
+      id = 1
     RETURNING
       *
   `;
