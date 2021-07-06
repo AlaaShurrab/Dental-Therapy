09c2e0ff457c59e3cda6021c7592658c00fc7c31
Osama Wael Younis <osama.w.younis@gmail.com>
2021-07-06 11:18:33 +0300

    add updated_at for appointments Relates #45

--- server/src/database/models/appointments/schema.sql
+++ server/src/database/models/appointments/schema.sql
@@ -6,5 +6,11 @@ CREATE TABLE "appointments" (
   "appointment_date" TEXT NOT NULL,
   "appointment_time" TIME NOT NULL,
   "notes" TEXT,
-  "created_at" TIMESTAMP NOT NULL DEFAULT NOW()
+  "created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
+  "updated_at" TIMESTAMP NOT NULL DEFAULT NOW()
 );
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON "appointments"
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
