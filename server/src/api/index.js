b85e055ac32089fd62202d32002619e315e25c3d
AlaaShurrab <alaa.m.shurrab98@gmail.com>
2021-07-10 20:34:59 +0300

    updating the api folder Relates #19

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import Boom from '@hapi/boom';
 import { errorMsgs } from '../services/error-handler';
-import { user, appointment, generalSettings } from '../modules';
+import { user, appointment, generalSettings, patients } from '../modules';
 
 const router = Router();
 
 router.use('/users', user.controllers);
 router.use('/appointments', appointment.controllers);
 router.use('/settings', generalSettings.controllers);
+router.use('/patients', patients.controllers);
 
 // catch 404 error and pass it to error handler
 router.use((req, res, next) => {
