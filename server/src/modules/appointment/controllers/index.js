3140c809a96a4a53084efd28a030e696cc2f4b4b
Osama Wael Younis <osama.w.younis@gmail.com>
2021-07-12 15:56:02 +0300

    just adding the patch /:id endpoint Relates #49

--- server/src/modules/appointment/controllers/index.js
+++ server/src/modules/appointment/controllers/index.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import createAppointments from './create-appointments';
 import availableAppointments from './available-appointments';
 import removeAppointments from './remove-appointments';
+import updateAppointments from './update-appointments';
 
 const router = Router();
 
 router.post('/', createAppointments);
 router.delete('/:id', removeAppointments);
+router.patch('/:id', updateAppointments);
 router.get('/available', availableAppointments);
 
 export default router;
