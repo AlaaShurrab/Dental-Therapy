3fc1bc2f4efc0b72ec5548d58d4693f3d86f8dad
Osama Wael Younis <osama.w.younis@gmail.com>
2021-07-16 21:34:21 +0300

    create get appointments controller Relates #53

--- server/src/modules/appointment/controllers/index.js
+++ server/src/modules/appointment/controllers/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
+import getAppointments from './get-appointments';
 import createAppointments from './create-appointments';
 import availableAppointments from './available-appointments';
 import removeAppointments from './remove-appointments';
 
 const router = Router();
 
+router.get('/', getAppointments);
 router.post('/', createAppointments);
 router.delete('/:id', removeAppointments);
 router.get('/available', availableAppointments);
